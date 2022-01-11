1179d4602cf736920b20c92c9d34cef3956d46bf
Killian Hascoet <killianh@live.fr>
2022-01-11 04:32:04 +0100

    Remove core js dep

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,12 @@
 module.exports = {
     presets: [
-        [
-            '@babel/preset-env',
-            {
-                useBuiltIns: 'usage',
-                corejs: 3
-            }
-        ]
-    ]
-}
+        ['@babel/preset-env', { targets: 'last 1 chrome version', modules: false }],
+    ],
+    env: {
+        test: {
+            presets: [
+                ['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 }],
+            ],
+        },
+    },
+};
