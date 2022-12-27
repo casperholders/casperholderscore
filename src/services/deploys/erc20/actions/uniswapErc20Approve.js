8da13c119c03f07a34c930fcd5b1851dd084b77f
Killian Hascoet <killianh@live.fr>
2022-12-27 02:03:21 +0100

    Add uniswap approve

---
+++ src/services/deploys/erc20/actions/uniswapErc20Approve.js
@@ -0,0 +1,52 @@
+import {
+  CLU256, RuntimeArgs,
+} from 'casper-js-sdk';
+import Erc20ApproveResult from '../../../results/erc20/erc20ApproveResult';
+import AbstractSmartContractStoredByHashDeployParameters
+  from '../../abstractSmartContractStoredByHashDeployParameters';
+
+/**
+ * @constant
+ * @type {string}
+ */
+const entrypoint = 'approve';
+
+/**
+ * @constant
+ * @type {number}
+ */
+const fee = 3500000000;
+
+/**
+ * Erc20Approve class
+ * Class used to create DeployParameters for an ERC20 Approve operation
+ */
+export default class UniswapErc20Approve extends AbstractSmartContractStoredByHashDeployParameters {
+  /**
+   * Constructor
+   *
+   * @param {string} activeKey - Current active key in the public hex format
+   * @param {string} amount - Amount of tokens that will be allowed to transfer
+   * @param {CLKey} spender - CLKey of the spender
+   * @param {string} network - Current network to execute the deployment
+   * @param {string} hash - Current hash of the stored SmartContract
+   * @param {number} ttl - Deploy time to live in hours
+   */
+  constructor(activeKey, amount, spender, network, hash, ttl = 1) {
+    const args = RuntimeArgs.fromMap({
+      spender,
+      amount: new CLU256(amount),
+    });
+    super(activeKey, network, hash, entrypoint, args, fee, ttl);
+  }
+
+  /**
+   * Get a DeployResult constructor
+   *
+   * @return {DeployResult.constructor} - Return the constructor of a given DeployResult
+   */
+  // eslint-disable-next-line class-methods-use-this
+  get deployResult() {
+    return Erc20ApproveResult;
+  }
+}
--- tests/erc20Operations.test.js
+++ tests/erc20Operations.test.js
@@ -1,10 +1,24 @@
 import { CLKey, CLPublicKey, DeployUtil } from 'casper-js-sdk';
-import { UniswapErc20Transfer } from '../src';
+import { UniswapErc20Approve, UniswapErc20Transfer } from '../src';
 import Erc20Approve from '../src/services/deploys/erc20/actions/erc20Approve';
 import Erc20Transfer from '../src/services/deploys/erc20/actions/erc20Transfer';
 
 test('ERC20 Approve Operation', async () => {
   const erc20ApproveDeployParameters = new Erc20Approve(
+    '0168e3a352e7bab76c85fb77f7c641d77096dae55845c79655522c24e9cc1ffe21',
+    '1',
+    new CLKey(CLPublicKey.fromHex('015acde48328ae5ae008ecd27a4671f7b967b5c89477d1315265e762c7f07fe52c')),
+    'casper-test',
+    'aee8a7ff16b770382aedf1a4cc83948613f315dc4f9560165f4feaa80fde5ac1',
+  );
+  const deploy = erc20ApproveDeployParameters.makeDeploy;
+  const result = DeployUtil.validateDeploy(deploy);
+  expect(result.ok)
+    .toBe(true);
+});
+
+test('Uniswap ERC20 Approve Operation', async () => {
+  const erc20ApproveDeployParameters = new UniswapErc20Approve(
     '0168e3a352e7bab76c85fb77f7c641d77096dae55845c79655522c24e9cc1ffe21',
     '1',
     new CLKey(CLPublicKey.fromHex('015acde48328ae5ae008ecd27a4671f7b967b5c89477d1315265e762c7f07fe52c')),
